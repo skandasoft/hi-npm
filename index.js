2a606b0f627212e1b9ff8eeeda0a519cbf29699e
Skandasoft <skandasoftinc@gmail.com>
2017-04-08 18:57:15 -0500

    beta version

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.export = function(){
-  console.log('hello npm!!!');
+module.exports = function(lang){
+  if(lang === 'es'){
+    console.log('hola npm');
+  }else{
+    console.log('hi npm');
+  }
 }
